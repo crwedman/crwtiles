6cd74296031c394ea8a828cc8e68391f1c2831db
crwedman <crwedman@gmail.com>
2026-03-11 11:06:34 -0600

    Use disjoint-set openings for WFC closure

--- CRWTILES/wfc.gd
+++ CRWTILES/wfc.gd
@@ -25,6 +25,100 @@ func _get_propagate_rules():
 		match_edges
 	]
 
+func _is_collapsed_open(cell: Cell) -> bool:
+	return cell != null and cell.is_collapsed() and not cell.tiles[0].is_solid()
+
+func _track_propagated_pos(queue: Array, pos: Vector3i) -> void:
+	if _propagated.has(pos):
+		return
+
+	_propagated[pos] = true
+	queue.push_back(pos)
+
+func _process_capped_frontier(observed_pos: Vector3i) -> void:
+	if not _cap_ends:
+		return
+
+	var queue = _propagated.keys()
+	_track_propagated_pos(queue, observed_pos)
+
+	var current = 0
+	while current < queue.size():
+		var pos: Vector3i = queue[current]
+		current += 1
+
+		var cell = at(pos)
+		if null == cell:
+			continue
+
+		for face in Face.FACES:
+			var neighbor_pos = offset(pos, face)
+			var had_neighbor = exists_at(neighbor_pos)
+			cap_ends(cell, pos, face)
+			if not had_neighbor and exists_at(neighbor_pos):
+				_track_propagated_pos(queue, neighbor_pos)
+
+func _all_positions() -> Dictionary:
+	var positions = {}
+	for pos in _cells.keys():
+		if exists_at(pos):
+			positions[pos] = true
+
+	for frame in _version_stack:
+		var frame_cells: Dictionary = frame[CELLS_VERSIONSTACK_INDEX]
+		for pos in frame_cells.keys():
+			if exists_at(pos):
+				positions[pos] = true
+
+	return positions
+
+func _collapsed_open_cells() -> Dictionary:
+	var collapsed_open = {}
+	for pos in _all_positions().keys():
+		var cell = at(pos)
+		if _is_collapsed_open(cell):
+			collapsed_open[pos] = cell
+
+	return collapsed_open
+
+func _build_open_components(collapsed_open: Dictionary) -> Dictionary:
+	for pos in collapsed_open.keys():
+		_propagated_set.find(pos)
+
+	for pos in collapsed_open.keys():
+		var cell: Cell = collapsed_open[pos]
+		var tile: Tile = cell.tiles[0]
+		for face in Face.FACES:
+			if tile.is_solid(face):
+				continue
+
+			var neighbor_pos = offset(pos, face)
+			var neighbor: Cell = collapsed_open.get(neighbor_pos)
+			if neighbor and not neighbor.tiles[0].is_solid(Face.OPPOSITE[face]):
+				_propagated_set.union(pos, neighbor_pos)
+
+	var component_openings = {}
+	for pos in collapsed_open.keys():
+		var cell: Cell = collapsed_open[pos]
+		var tile: Tile = cell.tiles[0]
+		var root = _propagated_set.find(pos)
+		if not component_openings.has(root):
+			component_openings[root] = 0
+
+		for face in Face.FACES:
+			if tile.is_solid(face):
+				continue
+
+			var neighbor_pos = offset(pos, face)
+			var neighbor = at(neighbor_pos)
+			if not _is_collapsed_open(neighbor):
+				component_openings[root] += 1
+				continue
+			if neighbor.tiles[0].is_solid(Face.OPPOSITE[face]):
+				component_openings[root] += 1
+
+	return component_openings
+
 func fuzz_tiles(cell: Cell, _pos: Vector3i, _face: int) -> bool:
 	cell._randomize_tiles(_rng)
 	return true
@@ -121,55 +215,47 @@ func _pre_propagate(_pos: Vector3i) -> bool:
 	return true
 
 func _post_propagate(observed_pos):
-	var to_update = _propagated.keys()
-	for pos in to_update:
-		var cell = at(pos)
-		if null == cell:
-			continue
-		for face in Face.FACES:
-			var neighbor_pos = offset(pos, face)
-			if cell.is_passable(face) and exists_at(neighbor_pos):
-				var neighbor = at(neighbor_pos)
-				if neighbor and neighbor.is_passable(Face.OPPOSITE[face]):
-					_propagated_set.union(pos, neighbor_pos)
-			if _cap_ends:
-				cap_ends(cell, pos, face)
+	_process_capped_frontier(observed_pos)
 
-	var tile: Tile = at(observed_pos).tiles[0]
-	for face in Face.FACES:
-		var neighbor_pos = observed_pos + Face.OFFSETS[face]
-		if not tile.is_solid(face) and exists_at(neighbor_pos):
-			var neighbor = at(neighbor_pos)
-			if neighbor and neighbor.is_passable(Face.OPPOSITE[face]):
-				_propagated_set.union(observed_pos, neighbor_pos)
+	var collapsed_open = _collapsed_open_cells()
+	if collapsed_open.is_empty():
+		return true
 
-	if _root_pos != null:
-		var root = _propagated_set.find(_root_pos)
-
-		if not tile.is_solid() and root != _propagated_set.find(observed_pos):
-			print("observed unreachable:%s" % [observed_pos])
-			#return false
-			pass # XXX
-
-		for pos in to_update:
-			var cell = at(pos)
-			var my_root = _propagated_set.find(pos)
-			if not cell.is_solid() and root != my_root:
-				#print("propagated unreachable:%s" % [pos])
-				#return false
-				#update(pos, [atlas.solid_tile])
-				pass # XXX
-
-		# finally, update the collapsed cell
-		for face in Face.FACES:
-			var neighbor_pos = observed_pos + Face.OFFSETS[face]
-			if not tile.is_solid(face) and exists_at(neighbor_pos):
-				var neighbor = at(neighbor_pos)
-				if neighbor and neighbor.is_passable(Face.OPPOSITE[face]):
-					_reachable_set.union(observed_pos, neighbor_pos)
-
-	if null == _root_pos:
-		_root_pos = observed_pos
+	var observed_cell = at(observed_pos)
+	if null == _root_pos or not collapsed_open.has(_root_pos):
+		if _is_collapsed_open(observed_cell):
+			_root_pos = observed_pos
+		else:
+			_root_pos = collapsed_open.keys()[0]
 		print("Root: %s" % [_root_pos])
+
+	var component_openings = _build_open_components(collapsed_open)
+	var root = _propagated_set.find(_root_pos)
+	var disconnected_components = {}
+
+	for pos in collapsed_open.keys():
+		var component = _propagated_set.find(pos)
+		if component != root:
+			disconnected_components[component] = true
+
+	if _cap_ends:
+		for component in disconnected_components.keys():
+			if component_openings.get(component, 0) == 0:
+				print("sealed disconnected component:%s" % [component])
+				return false
+
+		if component_openings.get(root, 0) == 0 and disconnected_components.size() > 0:
+			print("root component closed before reconnection")
+			return false
+
+	if len(_uncollapsed) == 0:
+		if disconnected_components.size() > 0:
+			print("final disconnected open components:%s" % [disconnected_components.size()])
+			return false
+		if component_openings.get(root, 0) > 0:
+			print("final map still open:%s" % [component_openings.get(root, 0)])
+			return false
+
+	_reachable_set = _propagated_set.duplicate()
 
 	return true
